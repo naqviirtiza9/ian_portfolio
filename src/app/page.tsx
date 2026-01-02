47b3747de368b22bbfea6f6c5874f672cc09f97c
irtiza naqvi <naqviirtiza9@gmail.com>
2026-01-02 14:26:03 +0500

    Full BentoGrid complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "../../components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import MyProjects from "@/components/MyProjects";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
             }/>
           <Hero/>
           <Grid/>
+          <MyProjects/>
         </h1>
       </div>
       </main>
